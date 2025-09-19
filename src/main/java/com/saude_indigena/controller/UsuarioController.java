8916dadda5e8f0e9bd72c90abaebf7749c240754
Vinicius79 <iliveiravinicius2503@gmail.com>
2025-09-18 21:50:27 -0400

    usuario controller

--- src/main/java/com/saude_indigena/controller/UsuarioController.java
+++ src/main/java/com/saude_indigena/controller/UsuarioController.java
@@ -1,8 +1,7 @@
 package com.saude_indigena.controller;
 
-import com.saude_indigena.dto.UsuarioCadastroDTO;
-import com.saude_indigena.dto.UsuarioListagemDTO;
-import com.saude_indigena.dto.UsuarioResponseDTO;
+import com.saude_indigena.dto.*;
+import com.saude_indigena.model.Pessoa;
 import com.saude_indigena.model.Usuario;
 import com.saude_indigena.model.mapper.UsuarioMapper;
 import com.saude_indigena.service.UsuarioService;
@@ -22,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/usuario")
@@ -56,6 +56,44 @@ public class UsuarioController {
         return ResponseApi.crudResponse(TipoResponseApi.INFO, Constantes.USUARIO_MSG_ADICIONADO, HttpStatus.CREATED, inspetorResponseDTO, uri);
     }
 
+    @Operation(summary = "Atualiza um usuario.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuario atualizada.",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ResponseApi.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "Parâmetros inválidos."),
+            @ApiResponse(responseCode = "401", description = "Não autorizado."),
+            @ApiResponse(responseCode = "403", description = "Acesso proibido."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor.")
+    })
+    @PutMapping("/{usuarioUuid}")
+    public ResponseEntity<Object> atualizar(@PathVariable UUID usuarioUuid, @RequestBody UsuarioAtualizacaoDTO dados){
+        Usuario usuario = this.usuarioService.atualizar(usuarioUuid, dados);
+        UsuarioResponseDTO response = this.usuarioMapper.toUsuarioResponseDTO(usuario);
+        return ResponseApi.crudResponse(TipoResponseApi.INFO, Constantes.USUARIO_MSG_LOCALIZADA, HttpStatus.NO_CONTENT, response, null);
+    }
+
+    @Operation(summary = "Busca um usuario.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario.",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ResponseApi.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "Parâmetros inválidos."),
+            @ApiResponse(responseCode = "401", description = "Não autorizado."),
+            @ApiResponse(responseCode = "403", description = "Acesso proibido."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor.")
+    })
+    @GetMapping("/{usuarioUuid}")
+    public ResponseEntity<Object> buscarPorUuid(@PathVariable UUID usuarioUuid) {
+        Usuario usuario = this.usuarioService.buscarPorUuid(usuarioUuid);
+        UsuarioResponseDTO response = this.usuarioMapper.toUsuarioResponseDTO(usuario);
+        return ResponseApi.crudResponse(TipoResponseApi.INFO, Constantes.USUARIO_MSG_LOCALIZADA, HttpStatus.OK, response, null);
+    }
+
     @Operation(summary = "Lista os usuarios.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Lista de usuarios.",
@@ -72,5 +110,23 @@ public class UsuarioController {
     public ResponseEntity<Object> listar(@PageableDefault Pageable pageable){
         List<UsuarioListagemDTO> lista = this.usuarioService.listar(pageable);
         return ResponseApi.crudResponse(TipoResponseApi.INFO, Constantes.USUARIO_MSG_LISTA, HttpStatus.OK, lista, null);
+    }
+
+    @Operation(summary = "Remove um usuario.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuario removido.",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ResponseApi.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "Parâmetros inválidos."),
+            @ApiResponse(responseCode = "401", description = "Não autorizado."),
+            @ApiResponse(responseCode = "403", description = "Acesso proibido."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor.")
+    })
+    @DeleteMapping("/{usuarioUuid}")
+    public ResponseEntity<Object> removerPessoa(@PathVariable UUID usuarioUuid) {
+        this.usuarioService.remover(usuarioUuid);
+        return ResponseApi.crudResponse(TipoResponseApi.INFO, Constantes.USUARIO_MSG_REMOVIDA, HttpStatus.NO_CONTENT, null, null);
     }
 }
--- src/main/java/com/saude_indigena/serviceImpl/AuthorizationService.java
+++ src/main/java/com/saude_indigena/serviceImpl/AuthorizationService.java
@@ -1,5 +1,7 @@
 package com.saude_indigena.serviceImpl;
 
+import com.saude_indigena.model.Usuario;
+import com.saude_indigena.repository.AdminRepository;
 import com.saude_indigena.repository.UsuarioRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,14 +11,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthorizationService implements UserDetailsService {
 
+    private final AdminRepository adminRepository;
+
     private final UsuarioRepository usuarioRepository;
 
-    public AuthorizationService(UsuarioRepository usuarioRepository) {
+    public AuthorizationService(AdminRepository adminRepository, UsuarioRepository usuarioRepository) {
+        this.adminRepository = adminRepository;
         this.usuarioRepository = usuarioRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return this.usuarioRepository.findByUsuario(username);
+        UserDetails admin = adminRepository.findByUsuario(username);
+        if (admin != null) {
+            return admin;
+        }
+
+        UserDetails user = usuarioRepository.findByUsuario(username);
+        if (user != null) {
+            return user;
+        }
+
+        throw new UsernameNotFoundException("Usuário não encontrado: " + username);
     }
 }
