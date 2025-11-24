9bbdcd18789704dedb1dd0be88712378e93e425c
Vinicius <vinicius.souza@manaus.am.gov.br>
2025-11-24 14:05:52 -0400

    campo sexo obrigatorio no cadastro de pessoa

--- src/main/java/com/saude_indigena/dto/PessoaCadastroDTO.java
+++ src/main/java/com/saude_indigena/dto/PessoaCadastroDTO.java
@@ -10,6 +10,7 @@ public record PessoaCadastroDTO(@NotBlank
                                 String nomeCompleto,
                                 @Size(min = 11, max = 11)
                                 String cpf,
+                                @NotBlank
                                 Sexo sexo,
                                 @JsonFormat(pattern = "dd/MM/yyyy")
                                 LocalDate dataNascimento,
